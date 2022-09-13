08d096531bc069440479605d543311793b56c9b9
Soumik Chatterjee <souchatt@outlook.in>
2022-09-13 23:59:46 +0530

    Small refactor to reuse code

--- src/test/java/com/tapedbanana/algorithms/SortTest.java
+++ src/test/java/com/tapedbanana/algorithms/SortTest.java
@@ -2,29 +2,30 @@ package com.tapedbanana.algorithms;
 
 import org.junit.jupiter.api.Test;
 
-import static com.tapedbanana.algorithms.Sort.*;
+import java.util.function.Consumer;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SortTest {
 
     @Test
     void bubbleSortWorks() {
-        int[] arr = new int[] {5, 1, 3, 2, 4};
-        bubbleSort(arr);
-        assertThat(arr).isSorted();
+        assertSortWorks(Sort::bubbleSort);
     }
 
     @Test
     void selectionSortWorks() {
-        int[] arr = new int[] {5, 1, 3, 2, 4};
-        selectionSort(arr);
-        assertThat(arr).isSorted();
+        assertSortWorks(Sort::selectionSort);
     }
 
     @Test
     void insertionSortWorks() {
+        assertSortWorks(Sort::insertionSort);
+    }
+
+    void assertSortWorks(Consumer<int[]> sort) {
         int[] arr = new int[] {5, 1, 3, 2, 4};
-        insertionSort(arr);
+        sort.accept(arr);
         assertThat(arr).isSorted();
     }
 }
